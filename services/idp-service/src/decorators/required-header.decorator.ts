a9fc6db38bef91e8d901ee6656a63910f7bc3060
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-01-04 13:03:20 +0530

    feat: add decorator for required header

--- services/idp-service/src/decorators/required-header.decorator.ts
+++ services/idp-service/src/decorators/required-header.decorator.ts
@@ -1,14 +1,11 @@
 import {ExecutionContext, createParamDecorator} from "@nestjs/common";
-import {MissingHeader} from "~/exceptions/missing-header.exception";
+import {Request} from "express";
+import {toHeader} from "~/utils/toHeader";
 
 export const RequiredHeader = createParamDecorator(function (
   headerName: string,
   ctx: ExecutionContext,
 ) {
   const request = ctx.switchToHttp().getRequest<Request>();
-  const header = request.headers[headerName];
-  if (header) {
-    return header;
-  }
-  throw new MissingHeader(headerName);
+  return toHeader(request, headerName);
 });
